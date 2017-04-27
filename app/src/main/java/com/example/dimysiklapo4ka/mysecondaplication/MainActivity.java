86f3e1afc778072159f4f046554ad38e839662ae
(init.dp.ua) Дмитрий Конышев <dimysiklapo4ka@gmail.com>
2017-04-27 18:57:23 +0300

    second commit

--- app/src/main/java/com/example/dimysiklapo4ka/mysecondaplication/MainActivity.java
+++ app/src/main/java/com/example/dimysiklapo4ka/mysecondaplication/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.dimysiklapo4ka.mysecondaplication;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private EditText editText2;
 
@@ -24,7 +26,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_main);
+
 
         editText2 = (EditText)findViewById(R.id.editText2);
 
@@ -42,20 +48,54 @@ public class MainActivity extends AppCompatActivity {
 
     public void buttonClick(View view){
 
+        String tmp = "";
+        String tmp2;
+        String tmpRez = "";
+        int countSum = 0;
+
     switch (view.getId()){
 
-        case R.id.btn_1:{
-            editText2.setText(editText2.getText().toString() + btn1.getText());
+        case R.id.btn_clear:{
+            if (editText2.length() != 0){
+                editText2.setText("");
+            }else {
+                editText2.setText("0");
+            }
             break;
         }
-        case R.id.btn_2:{
-            editText2.setText(editText2.getText().toString() + btn2.getText());
+        case R.id.btn_sum:{
+            countSum ++;
+            if (editText2.length() == 0){
+                editText2.setText("0");
+            }
+            else {
+                tmp = ("" + editText2.getText()).substring(0, editText2.getText().length() );
+            }
+            if (countSum > 1){
+                tmp2 = ("" + editText2.getText()).substring(tmp.length()+1, editText2.getText().length());
+                int tmpOne = Integer.parseInt(tmp);
+                int tmpTwo = Integer.parseInt(tmp2);
+                int rez = tmpOne + tmpTwo;
+                tmpRez = "" + rez;
+
+            }
+            editText2.setText("" + tmpRez + "+");
+            editText2.getText();
             break;
         }
-        case R.id.btn_3:{
-            editText2.setText(editText2.getText().toString() + btn3.getText());
+        case R.id.btn_equal:{
+            tmp2 = ("" + editText2.getText()).substring(tmp.length()+1, editText2.getText().length()-2);
+            int tmpOne = Integer.parseInt(tmp);
+            int tmpTwo = Integer.parseInt(tmp2);
+            int rez = tmpOne + tmpTwo;
+            editText2.setText("" + rez);
+            editText2.getText();
             break;
         }
+        default:{
+            editText2.setText("" + editText2.getText() + ((Button)view).getText());
+        }
+
     }
     }
 
